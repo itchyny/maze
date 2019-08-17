30115461ac5b79c67180aadf416c2ad1cca3fbf6
itchyny <itchyny@hatena.ne.jp>
2019-08-17 09:29:50 +0900

    fix main tests

--- cmd/maze/main_test.go
+++ cmd/maze/main_test.go
@@ -11,9 +11,12 @@ import (
 )
 
 func TestMain(t *testing.T) {
-	build, _ := filepath.Abs("../")
-	filepath.Walk("../test", func(path string, info os.FileInfo, err error) error {
-		if strings.HasSuffix(path, ".sh") {
+	build, _ := filepath.Abs("../../")
+	filepath.Walk("../../test", func(path string, info os.FileInfo, err error) error {
+		t.Run(filepath.Base(path), func(t *testing.T) {
+			if !strings.HasSuffix(path, ".sh") {
+				return
+			}
 			cmd := exec.Command("bash", filepath.Base(path))
 			cmd.Dir = filepath.Dir(path)
 			cmd.Env = append(os.Environ(), "PATH="+build+":"+"/bin")
@@ -21,19 +24,19 @@ func TestMain(t *testing.T) {
 			cmd.Stderr = stderr
 			output, err := cmd.Output()
 			if err != nil {
-				t.Errorf("FAIL: execution failed: " + path + ": " + err.Error() + " " + stderr.String())
+				t.Errorf("FAIL: execution failed: " + filepath.Base(path) + ": " + err.Error() + " " + stderr.String())
 			} else {
 				outfile := strings.TrimSuffix(path, filepath.Ext(path)) + ".txt"
 				expected, err := ioutil.ReadFile(outfile)
 				if err != nil {
 					t.Errorf("FAIL: error on reading output file: " + outfile)
 				} else if strings.HasPrefix(string(output), strings.TrimSuffix(string(expected), "\n")) {
-					t.Logf("PASS: " + path + "\n")
+					t.Logf("PASS: " + filepath.Base(path) + "\n")
 				} else {
-					t.Errorf("FAIL: output differs: " + path + "\n")
+					t.Errorf("FAIL: output differs: " + filepath.Base(path) + "\n")
 				}
 			}
-		}
+		})
 		return nil
 	})
 }
