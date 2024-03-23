4f3b3f000eaf11ba6d1f77fa0156168c06da9230
itchyny <itchyny@cybozu.co.jp>
2024-03-23 10:58:58 +0900

    add tests for image formats

--- cmd/maze/main_test.go
+++ cmd/maze/main_test.go
@@ -11,13 +11,13 @@ import (
 func TestMain(t *testing.T) {
 	build, _ := filepath.Abs("../../")
 	filepath.Walk("../../test", func(path string, info os.FileInfo, err error) error {
+		if !strings.HasSuffix(path, ".sh") {
+			return nil
+		}
 		t.Run(filepath.Base(path), func(t *testing.T) {
-			if !strings.HasSuffix(path, ".sh") {
-				return
-			}
 			cmd := exec.Command("bash", filepath.Base(path))
 			cmd.Dir = filepath.Dir(path)
-			cmd.Env = append(os.Environ(), "PATH="+build+":"+"/bin")
+			cmd.Env = append(os.Environ(), "PATH="+build+":/bin:/usr/bin")
 			var stderr strings.Builder
 			cmd.Stderr = &stderr
 			output, err := cmd.Output()
---
+++ test/image_svg.sh
@@ -0,0 +1,1 @@
+maze --seed 0 --width 10 --height 10 --scale 10 --image svg
